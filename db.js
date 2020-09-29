87772f2aec3df3ddcc5c34980b7b320b0a258d94
vagiPC <vagishyagnik@gmail.com>
2020-09-30 00:10:36 +0530

    JSON Done

--- db.js
+++ db.js
@@ -5,214 +5,214 @@ const db = new Sequelize({
     storage: __dirname + '/storage.db'
 })
 
-// const Employee_Table = db.define('Employee_Table',{
-//     Job_Title : {
-//         type : Sequelize.STRING,
-//         allowNull : false,
-//         unique : false
-//     },
-//     Name : {
-//         type : Sequelize.STRING,
-//         allowNull : false,
-//         unique: false
-//     },
-//     Emp_Id : {
-//         type : Sequelize.INTEGER,
-//         allowNull : false,
-//         unique : true,
-//         primaryKey: true
-//     },
-//     Phone_Number : {
-//         type : Sequelize.STRING,
-//         allowNull : false,
-//         unique : false
-//     },
-//     Res_Add : {
-//         type : Sequelize.STRING,
-//         allowNull : false,
-//         unique : false
-//     },
-//     Password : {
-//         type : Sequelize.STRING,
-//         allowNull : false,
-//         unique : false
-//     }
-// });
+const Employee_Table = db.define('Employee_Table',{
+    Job_Title : {
+        type : Sequelize.STRING,
+        allowNull : false,
+        unique : false
+    },
+    Name : {
+        type : Sequelize.STRING,
+        allowNull : false,
+        unique: false
+    },
+    Emp_Id : {
+        type : Sequelize.INTEGER,
+        allowNull : false,
+        unique : true,
+        primaryKey: true
+    },
+    Phone_Number : {
+        type : Sequelize.STRING,
+        allowNull : false,
+        unique : false
+    },
+    Res_Add : {
+        type : Sequelize.STRING,
+        allowNull : false,
+        unique : false
+    },
+    Password : {
+        type : Sequelize.STRING,
+        allowNull : false,
+        unique : false
+    }
+});
 
 
-// const Employee_Attendance = db.define('Employee_Attendance',{
-//     Emp_Id : {
-//         type : Sequelize.INTEGER,
-//         allowNull : false,
-//         unique : false,
-//         primaryKey: true
-//     },
-//     Entry_Date : {
-//         type : Sequelize.STRING,
-//         allowNull : false,
-//         unique: false,
-//         primaryKey: true
-//     },
-//     Entry_Time : {
-//         type : Sequelize.STRING,
-//         allowNull : false,
-//         unique : true
-//     },
-//     Exit_Time : {
-//         type : Sequelize.STRING,
-//         allowNull : false,
-//         unique : false
-//     }
-// });
+const Employee_Attendance = db.define('Employee_Attendance',{
+    Emp_Id : {
+        type : Sequelize.INTEGER,
+        allowNull : false,
+        unique : false,
+        primaryKey: true
+    },
+    Entry_Date : {
+        type : Sequelize.STRING,
+        allowNull : false,
+        unique: false,
+        primaryKey: true
+    },
+    Entry_Time : {
+        type : Sequelize.STRING,
+        allowNull : false,
+        unique : true
+    },
+    Exit_Time : {
+        type : Sequelize.STRING,
+        allowNull : false,
+        unique : false
+    }
+});
 
-// const Salary_Employee = db.define('Salary_Employee',{
-//     Job_Title : {
-//         type : Sequelize.STRING,
-//         allowNull : false,
-//         unique : true,
-//         primaryKey: true
-//     },
-//     Salary_Pm : {
-//         type : Sequelize.INTEGER,
-//         allowNull : false,
-//         unique: false
-//     },
-//     Monthly_Hours : {
-//         type : Sequelize.INTEGER,
-//         allowNull : false,
-//         unique : true
-//     }
-// }); 
+const Salary_Employee = db.define('Salary_Employee',{
+    Job_Title : {
+        type : Sequelize.STRING,
+        allowNull : false,
+        unique : true,
+        primaryKey: true
+    },
+    Salary_Pm : {
+        type : Sequelize.INTEGER,
+        allowNull : false,
+        unique: false
+    },
+    Monthly_Hours : {
+        type : Sequelize.INTEGER,
+        allowNull : false,
+        unique : true
+    }
+}); 
 
-// const Menu = db.define('Menu',{
-//     Item_Id : {
-//         type : Sequelize.INTEGER,
-//         allowNull : false,
-//         unique : false,
-//         primaryKey: true
-//     },
-//     Item_Name : {
-//         type : Sequelize.STRING,
-//         allowNull : false,
-//         unique: false
-//     },
-//     Item_Price : {
-//         type : Sequelize.INTEGER,
-//         allowNull : false,
-//         unique : true
-//     }
-// });
-
-
-// const Customer = db.define('Customer',{
-//     Username : {
-//         type : Sequelize.STRING,
-//         allowNull : false,
-//         unique : false,
-//         primaryKey: true
-//     },
-//     Name : {
-//         type : Sequelize.STRING,
-//         allowNull : false,
-//         unique: false
-//     },
-//     Password : {
-//         type : Sequelize.STRING,
-//         allowNull : false,
-//         unique : true
-//     },
-//     Current_Cart_Items : {
-//         type : Sequelize.JSON,
-//         allowNull : false,
-//         unique : false
-//     }
-// });
-
-// const Order = db.define('Order',{
-//     Order_Id : {
-//         type : Sequelize.INTEGER,
-//         allowNull : false,
-//         unique : false,
-//         primaryKey: true,
-//     },
-//     Username : {
-//         type : Sequelize.STRING,
-//         allowNull : false,
-//         unique: false
-//     },
-//     Payment_Amount : {
-//         type : Sequelize.STRING,
-//         allowNull : false,
-//         unique : true
-//     },
-//     Order_Data : {
-//         type : Sequelize.JSON,
-//         allowNull : false,
-//         unique : false
-//     }
-// });
-
-// const Reservation_Info = db.define('Reservation_Info',{
-//     Table_Id : {
-//         type : Sequelize.INTEGER,
-//         allowNull : false,
-//         unique : false,
-//         primaryKey: true
-//     },
-//     Username : {
-//         type : Sequelize.STRING,
-//         allowNull : false,
-//         unique: false,
-//         primaryKey: true
-//     },
-//     Start_Time : {
-//         type : Sequelize.STRING,
-//         allowNull : false,
-//         unique : false,
-//         primaryKey: true
-//     },
-//     End_Time : {
-//         type : Sequelize.STRING,
-//         allowNull : false,
-//         unique : false,
-//     },
-//     Members : {
-//         type : Sequelize.INTEGER,
-//         allowNull : false,
-//         unique : false
-//     }
-// });
-
-// const Payment_Info = db.define('Payment_Info',{
-//     Payment_Id : {
-//         type : Sequelize.INTEGER,
-//         allowNull : false,
-//         unique : true
-//     },
-//     Order_Id : {
-//         type : Sequelize.INTEGER,
-//         allowNull : false,
-//         unique: true
-//     },
-//     Payment_Date : {
-//         type : Sequelize.JSON,
-//         allowNull : false,
-//         unique : false
-//     },
-//     Payment_Amount : {
-//         type : Sequelize.INTEGER,
-//         allowNull : false,
-//         unique : false
-//     },
-//     Order_Data : {
-//         type : Sequelize.JSON,
-//         allowNull : false,
-//         unique : false
-//     }
-// });
+const Menu = db.define('Menu',{
+    Item_Id : {
+        type : Sequelize.INTEGER,
+        allowNull : false,
+        unique : false,
+        primaryKey: true
+    },
+    Item_Name : {
+        type : Sequelize.STRING,
+        allowNull : false,
+        unique: false
+    },
+    Item_Price : {
+        type : Sequelize.INTEGER,
+        allowNull : false,
+        unique : true
+    }
+});
 
 
-const Customer_Check = db.define('Customer_Check',{
+const Customer = db.define('Customer',{
+    Username : {
+        type : Sequelize.STRING,
+        allowNull : false,
+        unique : false,
+        primaryKey: true
+    },
+    Name : {
+        type : Sequelize.STRING,
+        allowNull : false,
+        unique: false
+    },
+    Password : {
+        type : Sequelize.STRING,
+        allowNull : false,
+        unique : true
+    },
+    Current_Cart_Items : {
+        type : Sequelize.JSON,
+        allowNull : false,
+        unique : false
+    }
+});
+
+const Order = db.define('Order',{
+    Order_Id : {
+        type : Sequelize.INTEGER,
+        allowNull : false,
+        unique : false,
+        primaryKey: true,
+    },
+    Username : {
+        type : Sequelize.STRING,
+        allowNull : false,
+        unique: false
+    },
+    Payment_Amount : {
+        type : Sequelize.STRING,
+        allowNull : false,
+        unique : true
+    },
+    Order_Data : {
+        type : Sequelize.JSON,
+        allowNull : false,
+        unique : false
+    }
+});
+
+const Reservation_Info = db.define('Reservation_Info',{
+    Table_Id : {
+        type : Sequelize.INTEGER,
+        allowNull : false,
+        unique : false,
+        primaryKey: true
+    },
+    Username : {
+        type : Sequelize.STRING,
+        allowNull : false,
+        unique: false,
+        primaryKey: true
+    },
+    Start_Time : {
+        type : Sequelize.STRING,
+        allowNull : false,
+        unique : false,
+        primaryKey: true
+    },
+    End_Time : {
+        type : Sequelize.STRING,
+        allowNull : false,
+        unique : false,
+    },
+    Members : {
+        type : Sequelize.INTEGER,
+        allowNull : false,
+        unique : false
+    }
+});
+
+const Payment_Info = db.define('Payment_Info',{
+    Payment_Id : {
+        type : Sequelize.INTEGER,
+        allowNull : false,
+        unique : true
+    },
+    Order_Id : {
+        type : Sequelize.INTEGER,
+        allowNull : false,
+        unique: true
+    },
+    Payment_Date : {
+        type : Sequelize.JSON,
+        allowNull : false,
+        unique : false
+    },
+    Payment_Amount : {
+        type : Sequelize.INTEGER,
+        allowNull : false,
+        unique : false
+    },
+    Order_Data : {
+        type : Sequelize.JSON,
+        allowNull : false,
+        unique : false
+    }
+});
+
+
+const CustomerTemp = db.define('CustomerTemp',{
     Username : {
         type : Sequelize.STRING,
         allowNull : false,
@@ -243,13 +243,11 @@ db.sync().then(()=>{
 
 
 module.exports = {
-    // Employee_Attendance,
-    // Employee_Table,
-    // Salary_Employee,
-    // Order,
-    // Menu,
-    // Payment_Info,
-    // Reservation_Info,
-    // Customer
-    Customer_Check
+    Employee_Attendance,
+    Employee_Table,
+    Salary_Employee,
+    Order,
+    Menu,
+    Payment_Info,
+    Reservation_Info
 }
